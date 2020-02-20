ea91ea4f1a83a57de2874e92e4aec1c099e58c2c
saurabh Agrawal <sauraag2@cisco.com>
2020-02-20 15:32:17 +0530

    first commit

---
+++ src/main/java/com/stackssimplify/restservices/SpringbootBuldingblocks1Application.java
@@ -0,0 +1,13 @@
+package com.stackssimplify.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuldingblocks1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuldingblocks1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/stackssimplify/restservices/SpringbootBuldingblocks1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackssimplify.restservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBuldingblocks1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
