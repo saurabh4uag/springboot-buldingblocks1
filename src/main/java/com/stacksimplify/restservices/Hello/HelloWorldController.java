172946e71dd951ec0f460c4e413db69b23f5700e
saurabh Agrawal <sauraag2@cisco.com>
2020-02-25 14:17:58 +0530

    base code

--- src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
+++ src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-	//Simple Method
+	
 	//URI - /helloworld
 	//GET 
 	//@RequestMapping(method = RequestMethod.GET, path = "/helloworld")
